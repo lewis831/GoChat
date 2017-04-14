c969dfb6de95fe59fbaeb6a287a58ac90d6fffb0
lewis831 <lewisrashe@Lewiss-MacBook-Air.local>
2017-04-14 03:33:46 -0700

    Starting work on anonymous login with Firebase.

--- GoChat/AppDelegate.swift
+++ GoChat/AppDelegate.swift
@@ -6,19 +6,24 @@
 //  Copyright © 2017 Lewis Rashe. All rights reserved.
 //
 
+//To connect Firebase when your app starts up, add the initialization code below to your main AppDelegate class.
+//<Firabase code snippet start>
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+    
+    private func application(application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?)
+        -> Bool {
+            FIRApp.configure()
+            return true
     }
-
+//<Firebase code snippet end>
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
