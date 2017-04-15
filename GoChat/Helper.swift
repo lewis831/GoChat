4ebdd8b1d44e941a2acf75a52382cba879dd218c
lewis831 <lewisrashe@Lewiss-MacBook-Air.local>
2017-04-15 16:06:16 -0700

    func loginAnonymouslyDidTapped fixed

---
+++ GoChat/Helper.swift
@@ -0,0 +1,14 @@
+//
+//  Helper.swift
+//  GoChat
+//
+//  Created by Lewis Rashe on 4/15/17.
+//  Copyright © 2017 Lewis Rashe. All rights reserved.
+//
+
+import Foundation
+
+class Helper {
+    static let helper = Helper()
+    
+}
--- GoChat/LoginViewController.swift
+++ GoChat/LoginViewController.swift
@@ -35,29 +35,30 @@ class LoginViewController: UIViewController {
         //switch view by setting navigation controller as root view controller
         
         //Anonymous user authetication with Firebase
-        FIRAuth.auth()?.signInAnonymously(completion: { (anonymousUser: FIRUser?, error: NSError?) in
+        FIRAuth.auth()?.signInAnonymously(completion: { (anonymousUser, error) in
             if error == nil {
+                //Displays UserId in console
                 print("UserId: \(anonymousUser!.uid)")
+                
+                //Create a main storyboard instance
+                let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                
+                //From main storyboard instantiate a navigation controller
+                //To connect this code go to Main.storyboard -> Navigation Controller -> identity inspector -> Storyboard ID -> enter value: "NavigationVC"
+                let naviVC = storyboard.instantiateViewController(withIdentifier: "NavigationVC") as! UINavigationController
+                
+                //Get the app delegate
+                let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                
+                //Set Navigation Controller as root view controller
+                appDelegate.window?.rootViewController = naviVC
+                
             } else {
                 print(error!.localizedDescription)
                 return
             }
-        } as? FIRAuthResultCallback)
-        
-        
-        //Create a main storyboard instance
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        
-        //From main storyboard instantiate a navigation controller
-        //To connect this code go to Main.storyboard -> Navigation Controller -> identity inspector -> Storyboard ID -> enter value: "NavigationVC"
-        let naviVC = storyboard.instantiateViewController(withIdentifier: "NavigationVC") as! UINavigationController
-        
-        //Get the app delegate
-        let appDelegate = UIApplication.shared.delegate as! AppDelegate
-        
-        //Set Navigation Controller as root view controller
-        appDelegate.window?.rootViewController = naviVC
-            }
+        })
+    }
     
     @IBAction func googleLoginDidTapped(_ sender: Any) {
         print("google login did tapped")
