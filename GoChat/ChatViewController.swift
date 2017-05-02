87afe33b189aff2e983dfd01ba5af43fdbb99e65
lewis831 <lewisrashe@Lewiss-MacBook-Air.local>
2017-05-01 21:50:45 -0700

    Small update

--- GoChat/ChatViewController.swift
+++ GoChat/ChatViewController.swift
@@ -14,6 +14,7 @@ import FirebaseDatabase
 import FirebaseStorage
 import FirebaseAuth
 import SDWebImage
+
 class ChatViewController: JSQMessagesViewController {
     
     var messages = [JSQMessage]()
